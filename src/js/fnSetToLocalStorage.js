7670434c8248492a7f302cc604b18e459ca4142a
Helen <najdenova.elena@gmail.com>
2021-01-29 01:36:41 +0200

    добавила функционал на все кнопки (library, add to..., watched, queue)

---
+++ src/js/fnSetToLocalStorage.js
@@ -0,0 +1,15 @@
+import notify from './notifyFunction.js';
+
+function setToLocalStorage(id, localStorageKeyName, listName) {
+  let newValue = [];
+  let checkedKeyValue = localStorage.getItem(`${localStorageKeyName}`); // assigns a key's value
+  if (checkedKeyValue) {
+    newValue = checkedKeyValue.split(',');
+  }
+  if (!newValue.includes(id)) {
+    newValue.push(id);
+    notify(`${listName}`);
+  }
+  localStorage.setItem(`${localStorageKeyName}`, newValue);
+}
+export default setToLocalStorage;
