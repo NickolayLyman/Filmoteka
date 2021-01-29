66cf8a549fd89a17a6f0d3a1544ce7857f1275d6
Helen <najdenova.elena@gmail.com>
2021-01-29 19:15:48 +0200

    add buttons and link style in the header section

--- src/js/updateMarkup.js
+++ src/js/updateMarkup.js
@@ -4,11 +4,11 @@ import refs from './refs';
 import movieCardsTpl from '../template/movieCards.hbs';
 
 function updateMarkup(movie) {
-    let markup = '';
-    if (movie.status !== '404') {
-        markup = movieCardsTpl(movie);
-    } 
-    refs.gallery.insertAdjacentHTML('beforeend', markup);
+  let markup = '';
+  if (movie.status !== '404') {
+    markup = movieCardsTpl(movie);
+  }
+  refs.gallery.insertAdjacentHTML('beforeend', markup);
 }
 
 export default updateMarkup;
