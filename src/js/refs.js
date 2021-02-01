a391299a0844dcca7d3bd692f0fb1158982cf81d
s1eepingbeauty <kingz.bridge@gmail.com>
2021-02-02 01:31:31 +0200

    update styles for adaptive modal window

--- src/js/refs.js
+++ src/js/refs.js
@@ -9,15 +9,16 @@ const searchBySelector = querySelector => {
 };
 
 const refs = {
+  body: document.querySelector('body'),
+  upBtn: document.querySelector('.up-btn-wrapper'),
   gallery: document.querySelector('#gallery'),
   form: document.querySelector('.search-form'),
   modalDiv: document.querySelector('.js-lightbox'),
+  lightbox: document.querySelector('.lightbox'),
   overlayDiv: document.querySelector('.lightbox__overlay'),
   modalDivContent: document.querySelector('.lightbox__content'),
-  openImg: document.querySelector('.lightbox__image'),
   libraryButton: document.querySelector('.library-ref'),
   container: document.getElementById('.container'),
-  lightbox: document.querySelector('.lightbox'),
   header: document.querySelector('#page-header'),
   galleryList: document.querySelector('.gallery'),
   watchedBtn: document.querySelector('#watched-film-btn'),
