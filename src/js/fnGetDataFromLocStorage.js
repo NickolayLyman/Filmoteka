a023aaf54b10740e3b1f9afde8a0b5116ac1644e
Elena Naydenova <helen@localhost.localdomain>
2021-02-03 17:32:43 +0200

    add pagination styles and modal window style

--- src/js/fnGetDataFromLocStorage.js
+++ src/js/fnGetDataFromLocStorage.js
@@ -38,14 +38,6 @@ async function getMoviesFromLocalStorage(key) {
   }
 
   addEmptyStylePage();
-  // if (!refs.galleryList.hasChildNodes()) {
-  //   //console.log('hello');
-  //   refs.gallery.innerHTML = '';
-  //   refs.galleryList.classList.add('empty-page-style');
-  //   refs.galleryList.classList.remove('gallery');
-  //   refs.gallery.innerHTML =
-  //     '<li><h2>EMPTY<h2><p>Nothing has been added yet :(</p><img class="image" data-empty-page="empty-page" src="https://i0.wp.com/www.ecommerce-nation.com/wp-content/uploads/2017/08/How-to-Give-Your-E-Commerce-No-Results-Page-the-Power-to-Sell.png?fit=1000%2C600&ssl=1" alt="empty page pic"></li>';
-  // }
 
   refs.gallery.addEventListener("click", () => {
     if (event.target.classList.contains("delMovieBtn")) {
