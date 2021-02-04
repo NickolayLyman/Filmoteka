23d6e519983eaafcaaf4950e64dd5ef3dc3d4210
oleksiy-kondakov <htconex011212@gmail.com>
2021-02-04 20:31:33 +0200

    исправил функции на скролл и закрытие модалки

--- src/js/teamModal.js
+++ src/js/teamModal.js
@@ -1,3 +1,4 @@
+import refs from './refs';
 (() => {
   const btns = {
     openModalBtn: document.querySelector("[data-modal-open]"),
@@ -11,23 +12,24 @@
       btns.modal.classList.remove("is-hidden");
       window.addEventListener("keydown", onPressEscape);
       btns.closeModalBtn.addEventListener("click", closeModal);
-      btns.modal.addEventListener("click", closeModal);
+    btns.modal.addEventListener("click", backdropCloseModal);
+    refs.body.classList.add('scroll-hidden');
     };
-  function closeModal(event) {
-    console.log(event);
-    if (event.srcElement.className === "team-modal") {
-      return
-    } else{
-
+  function closeModal() {
+refs.body.classList.remove('scroll-hidden');
     btns.closeModalBtn.removeEventListener("click", closeModal);
     btns.modal.classList.add("is-hidden");
     btns.modal.removeEventListener("click", closeModal);
-  window.removeEventListener('keydown', onPressEscape);}
-  
+  window.removeEventListener('keydown', onPressEscape);
   }
   function onPressEscape(event) {
     if (event.code === 'Escape') {
       closeModal();
     }
+  }
+  function backdropCloseModal(event) {
+    if (event.currentTarget === event.target) {
+    closeModal();
+  }
   }
 })();
