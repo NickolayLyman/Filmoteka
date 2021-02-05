6fed0f78b096ac520f3e08470ca9dfde1200005f
LiudmylaKostova <kostolu@ukr.net>
2021-02-05 13:34:00 +0200

    Dark Theme fixed bug

--- src/index.js
+++ src/index.js
@@ -1,11 +1,7 @@
 import './js/teamModal';
 import './scss/main.scss';
 import './js/fetchMovies';
-<<<<<<< Updated upstream
 import './js/cnahgeTheme';
-=======
-import './js/changeTheme';
->>>>>>> Stashed changes
 import './js/modalCards'
 // import './js/spinner';
 import './js/script.js';
