6ed045c8f491f39e918c7d75652aec0e0c0312c7
s1eepingbeauty <kingz.bridge@gmail.com>
2021-02-07 00:37:34 +0200

    update

--- src/js/Auth.js
+++ src/js/Auth.js
@@ -22,12 +22,13 @@ function initApp(callback) {
       // User is signed in.
       const displayName = user.displayName;
       const photoURL = user.photoURL;
+      const email = user.email;
       const uid = user.uid;
 
       refs.signOut.hidden = false;
       refs.signIn.hidden = true;
       refs.userInfo.hidden = false;
-          refs.userInfo.innerHTML =
+      refs.userInfo.innerHTML =
             `<img class="user-img" src="${photoURL}"> 
             <div class="user-block">
               <p class="user-name">${displayName}</p>
