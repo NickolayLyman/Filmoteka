2f3ba69f1a3ce3924b48066f20826dbe9a201e08
Vitalii Skubii <svorder@ukr.net>
2021-02-07 10:16:36 +0200

    Кнопка load more

--- src/js/fetchSearchedMovies.js
+++ src/js/fetchSearchedMovies.js
@@ -1,15 +1,28 @@
-import { currentPage } from './pagination'
+export default {
+    searchQuery: '',
+    currentPage: 1,
+    fetchSearchedMovie() {
+        
+        const apiKey = "api_key=50b81e1c6c3b9e5f74d2015b742ff0b0";
+        const url = `https://api.themoviedb.org/3/search/movie?${apiKey}&language=en-US&page=${this.currentPage}&include_adult=false&query=${this.searchQuery}`;
+        return fetch(url).then(res => res.json()).then(({ results }) => {
+            this.incrementPage();
+            return results;
+        });
+            
+        
+    },
+    resetCurrentPage() {
+        this.currentPage = 1;
+    },
+    incrementPage() {
+        this.currentPage += 1;
+    },
+    get query() {
+        return this.searchQuery;
+    },
+    set query(value) {
+        return this.searchQuery = value;
+    },
 
-const apiKey = "api_key=50b81e1c6c3b9e5f74d2015b742ff0b0";
-
-function fetchSearchedMovie(searchMovie) {
-    const url = `https://api.themoviedb.org/3/search/movie?${apiKey}&language=en-US&page=${currentPage}&include_adult=false&query=${searchMovie}`;
-    
-    return fetch(url)
-        .then(response => response.json())
-        .catch(error => console.log(error));
-    
-    
-}
-
-export default fetchSearchedMovie;
+};
--- src/js/markupSearchedMovies.js
+++ src/js/markupSearchedMovies.js
@@ -1,4 +1,5 @@
-import fetchSearchedMovie from './fetchSearchedMovies';
+ import API from './fetchSearchedMovies';
+// import API from './API'
 import updateMarkup from './updateMarkup'
 import refs from './refs';
 import { error, empty } from './notifyFunction';
@@ -7,51 +8,83 @@ import { updateMovieMarkup } from './fetchMovies';
 import { Spinner } from 'spin.js';
 import opts from './spinner'
 
-let query = '';
-
-
-
-
-function markupSearchedMovie() {
-  refs.form.addEventListener('submit', event => {
+function markupSearchedMovie(){
+refs.form.addEventListener('submit', event => {
     event.preventDefault();
-   
-    query = event.currentTarget.elements.search.value;
-    console.log(query);
-    refs.gallery.innerHTML = '';
+
+    API.searchQuery = event.currentTarget.elements.search.value;
+     refs.pagination.classList.add('visually-hidden')
+refs.gallery.innerHTML = '';
     if (refs.input.value === ''){
-      refs.pagination.classList.add('visually-hidden')
-      empty();
-      refs.gallery.innerHTML = '';
-      return;
+empty();
+    return;
     }
 
-     var target = document.getElementById('gallery');
-    var spinner = new Spinner(opts).spin(target);
-    
-    fetchSearchedMovie(query).then(({ results }) => {
-      console.log('search', { results })
+    event.currentTarget.reset();
 
-      if (results.length === 0) {
+    API.resetCurrentPage();
+
+
+    fetchAPI();
+   
+    // refs.loadMoreBtn.classList.add('is-hidden');
+    // API.fetchImg().then(hits => {
+    //     renderImg(hits);
+    //     refs.loadMoreBtn.classList.remove('is-hidden')
+        
+    // });
+   
+});}
+
+refs.loadMoreBtn.addEventListener('click', fetchAPI);
+
+//     () => {
+//         API.fetchImg().then(hits => {
+//         renderImg(hits);
+        
+//     });
+// });
+    
+function fetchAPI() {
+    
+    refs.loadMoreBtn.classList.add('is-hidden');
+
+    var target = document.getElementById('gallery');
+    var spinner = new Spinner(opts).spin(target);
+
+    API.fetchSearchedMovie().then(results => {
+        if (results.length === 0) {
         error();
-        refs.pagination.classList.add('visually-hidden')
-        spinner.stop();
+spinner.stop();
         return
 
       }
-      refs.pagination.classList.remove('visually-hidden')
-      fetchGenres().then(({ genres }) => {
+            
+        
+        
+        fetchGenres().then(({ genres }) => {
         updateMovieMarkup(results, genres);
 
       }).finally(() => {
         spinner.stop();
       });
-      
-    }
-    )
-  })
+        // if (!renderImg(hits)) { 
+        //     error({
+        //     delay: 1000,
+        //     text: 'Img not found!',
+        //     })
+            
+        // }
+        refs.loadMoreBtn.classList.remove('is-hidden');
+        // window.scrollTo({
+        //     top: document.documentElement.scrollHeight,
+        //         behavior: 'smooth',
+        // });
+        }
+                       
+        
+    );
+    
 }
 
 export default markupSearchedMovie;
-
-export { query };
